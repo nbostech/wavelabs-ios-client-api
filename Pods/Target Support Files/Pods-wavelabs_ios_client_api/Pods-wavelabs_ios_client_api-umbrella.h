59713ade66e3dc3ee19e0760e8000239d19c6152
afsara <afsarunnisa@nbostech.com>
2016-03-01 17:30:53 +0530

    initial commit

---
+++ Pods/Target Support Files/Pods-wavelabs_ios_client_api/Pods-wavelabs_ios_client_api-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_wavelabs_ios_client_apiVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_wavelabs_ios_client_apiVersionString[];
+
---
+++ wavelabs_ios_client_api/wavelabs_ios_client_api.h
@@ -0,0 +1,19 @@
+//
+//  wavelabs_ios_client_api.h
+//  wavelabs_ios_client_api
+//
+//  Created by afsarunnisa on 3/1/16.
+//  Copyright (c) 2016 Wavelabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for wavelabs_ios_client_api.
+FOUNDATION_EXPORT double wavelabs_ios_client_apiVersionNumber;
+
+//! Project version string for wavelabs_ios_client_api.
+FOUNDATION_EXPORT const unsigned char wavelabs_ios_client_apiVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <wavelabs_ios_client_api/PublicHeader.h>
+
+
